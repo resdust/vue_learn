b9f3998ea609966e491026b90625520c635588ee
resdust winter <resdust_li@163.com>
2021-11-09 18:12:33 +0800

    更新到props配置

---
+++ 19_单文件组件/test_app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="container">
+  <student name="Jack" gender="male" :age="18"/>
+</div>
+  
+</template>
+
+<script>
+import Student from './components/Student.vue'
+
+export default {
+  name:"App",
+  components:{
+    Student
+  },
+  data() {
+    return {
+      msg:"欢迎学习Vue！"
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
